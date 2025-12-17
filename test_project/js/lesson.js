9c39f34a94a4728d81b7c59291f288c5b2c300e6
jumalievvnurmuha <jumalievnurmuha@gmail.com>
2025-12-17 20:51:52 +0600

    Prev

--- test_project/js/lesson.js
+++ test_project/js/lesson.js
@@ -149,3 +149,12 @@ btnPrev.onclick = () => {
       `;
     });
 };
+
+fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(response => response.json()) 
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Ошибка при запросе:', error);
+  });
